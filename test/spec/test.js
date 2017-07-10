d3613009b5167a9ba6079b0b3ba80eacf7829b65
Marty <marty.stepien@gmail.com>
2017-07-10 11:59:42 +0100

    test updated

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,21 +3,17 @@
 
   describe('Calculator', function () {
     describe('same math operations', function () {
-      var Calc = new Calculator;
       it('should add two numbers and return the sum of them', function () {
-        expect(Calc.sum([2,4])).to.equal(6);
+        expect(add(2,4)).to.equal(6);
       });
       it('should substract two numbers and return the difference of them', function () {
-        expect(Calc.substract([9,4])).to.equal(5);
+        expect(subtract(9,4)).to.equal(5);
       });
       it('should multiply two numbers and return the result', function () {
-        expect(Calc.multiply([2,6])).to.equal(12);
+        expect(multiply(2,6)).to.equal(12);
        });
        it('should divide two numbers and return the result', function () {
-         expect(Calc.divide([24,8])).to.equal(3);
-       });
-       it('should add three numbers and return the sum of them', function () {
-         expect(Calc.sum([2,4,3])).to.equal(9);
+         expect(divide(24,8)).to.equal(3);
        });
     });
   });
