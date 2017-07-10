e8573b96b9bd3a2c44ebab0cf647c0c8ea801719
Marty <marty.stepien@gmail.com>
2017-07-10 10:18:04 +0100

    tests added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('\'Allo \'Allo!');
+class Calculator {
+  constructor() {}
+
+  sum(args) {
+    return args.reduce(function(a,b) {
+      return a + b;
+    }
+    )}
+
+  substract(args) {
+    return args.reduce(function(a, b) {
+      return a - b;
+    })
+  }
+
+  multiply(args) {
+    return args.reduce(function(a, b) {
+      return a * b;
+    })
+  }
+
+  divide(args) {
+    return args.reduce(function(a, b) {
+      return a / b;
+    })
+  }
+}
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,10 +1,29 @@
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-
+  describe('Calculator', function () {
+    describe('same math operations', function () {
+      var Calc = new Calculator;
+      it('should add two numbers and return the sum of them', function () {
+        expect(Calc.sum([2,4])).to.equal(6);
+      });
+      it('should substract two numbers and return the difference of them', function () {
+        expect(Calc.substract([9,4])).to.equal(5);
+      });
+      it('should multiply two numbers and return the result', function () {
+        expect(Calc.multiply([2,6])).to.equal(12);
+       });
+       it('should divide two numbers and return the result', function () {
+         expect(Calc.divide([24,8])).to.equal(3);
+       });
+       it('should add three numbers and return the sum of them', function () {
+         expect(Calc.sum([2,4,3])).to.equal(9);
+       });
+    });
+    describe('various math operations', function () {
+      var Calc = new Calculator;
+      it('it should add two numbers and substract the third', function() {
+        expect(Calc.)
       });
     });
   });
